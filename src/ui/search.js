01c702874b071593fd9db951edd179891f6c7370
bob <robertwalks@gmail.com>
2020-08-12 08:23:40 -0400

    edit: fuzzysort works well for symbol lookups

---
+++ src/ui/search.js
@@ -0,0 +1,16 @@
+import fs from 'fs'
+
+import fuzzysort from 'fuzzysort'
+
+let symbolFile = fs.readFileSync('data/sym-name.json', 'utf8')
+symbolFile = JSON.parse(symbolFile)
+
+export function search(what) {
+  const results = fuzzysort.go(what, symbolFile, {
+    keys: ['name', 'symbol'],
+    limit: 10,
+    allowTypo: true,
+  })
+
+  return results
+}
