f95a21ffd52bb099223aad87939bf8b4f5a6cfae
bob <robertwalks@gmail.com>
2020-08-25 11:51:24 -0400

    edit: help

--- src/ui/help.js
+++ src/ui/help.js
@@ -17,45 +17,77 @@ try {#cd2-fg}help <command>{/} for command specific help
   e.g. {#cd2-fg}help :{/}
        {#cd2-fg}help \${/}
 
+{#bf6-fg}go back{/} to repl with {#cd2-fg}>{/}
+{red-fg}close{/} this window with {#cd2-fg}x{/}
+or use {#cd2-fg}tab/Shift-tab{/} to rotate through components
+
 {#eb4-fg}scroll down for more...{/}
 
 {bold}available commands:{/}
+{#bf6-fg}---------------------{/}
 {#cd2-fg}h(elp){/}   :prints this menu
+{#bf6-fg}---------------------{/}
 {#cd2-fg}[{/}        :target (window) prefix
          :changes the active window
+{#bf6-fg}---------------------{/}
 {#cd2-fg}[new{/}     :open new window in workspace
+{#bf6-fg}---------------------{/}
 {#cd2-fg}[all{/}     :target all windows in workspace
+{#bf6-fg}---------------------{/}
 {#cd2-fg}x{/}        :close active window
+{#bf6-fg}---------------------{/}
 {#cd2-fg}\${/}        :ticker (symbol) prefix
          :changes the active ticker symbol
+{#bf6-fg}---------------------{/}
 {#cd2-fg}:{/}        :time (range) prefix
          :changes the active time range
+{#bf6-fg}---------------------{/}
 {#cd2-fg}#/chart{/}  :display chart in targeted window
+{#bf6-fg}---------------------{/}
 {#cd2-fg}%{/}        :technical (indicator) prefix
          :adds a technical indicator to targeted chart window 
+         :{#f5c-fg}(paid iex subscribers only){/}
+{#bf6-fg}---------------------{/}
+{#cd2-fg}^/book{/}        :book
+         :display order book in the active window
+{#bf6-fg}---------------------{/}
 {#cd2-fg}"/quote{/}        :quote
          :display quote in the active window
+{#bf6-fg}---------------------{/}
 {#cd2-fg}!/news{/}        :news
          :display news in the active window
+{#bf6-fg}---------------------{/}
 {#cd2-fg}=/watchlist{/}        :watchlist
          :display watchlist in the active window
+{#bf6-fg}---------------------{/}
 {#cd2-fg}&/profile{/}:profile
          :display profile in the active window
+{#bf6-fg}---------------------{/}
 {#cd2-fg}*/list{/}   :list
          :display active/gainers/losers list in the active window
+{#bf6-fg}---------------------{/}
+{#cd2-fg}sectors{/}  :display sectors info in the active window
+{#bf6-fg}---------------------{/}
+{#cd2-fg}poll{/}     :poll (milliseconds) prefix
+         :sets polling to (milliseconds) in the target window
+         :enter {#cd2-fg}poll{/} with no time argument to stop polling.
+         :ex: {#cd2-fg}poll1e4{/} poll every second
+         :ex: {#cd2-fg}[1 poll{/} stop polling in window 1
+{#bf6-fg}---------------------{/}
 {#cd2-fg}?{/}        :search (terms)
+{#bf6-fg}---------------------{/}
 {#cd2-fg}@/account{/}:account
          :display iex/alpaca account info in the active window
 
-------------------
+{#bf6-fg}-----------------------------{/}
 
 {bold}Alpaca commands{/}
-{#cd2-fg}@{/}        :account {#f6a-fg}requires alpaca keys{/}
-{#cd2-fg}+{/}        :buy (quantity) (symbol) {#f6a-fg}requires alpaca keys{/}
-{#cd2-fg}-{/}        :sell (quantity) (symbol){#f6a-fg}requires alpaca keys{/}
+{#cd2-fg}@{/}        :account {#f5c-fg}requires alpaca keys{/}
+{#cd2-fg}+{/}        :buy (quantity) (symbol) {#f5c-fg} requires alpaca keys{/}
+{#cd2-fg}-{/}        :sell (quantity) (symbol) {#f5c-fg} requires alpaca keys{/}
 {#cd2-fg}exit / quit / <Ctrl-c>{/}     :quit iexcli
 
-{#fcc-fg}-----------------------------{/}
+{#bf6-fg}-----------------------------{/}
 
 commands can be aggregated:
 
@@ -100,6 +132,7 @@ export function halp(ws) {
 export function help(ws, words) {
   // print specific help if applicable
   let what = words[words.findIndex((w) => /h(elp)?/.test(w)) + 1]
+
   if (what) {
     const whatSym = `
     {bold}{#2ea-fg}help {#cd2-fg}\${/} (symbol)
@@ -120,6 +153,10 @@ Display chart. Can be combined with time and stock prefixes to update multiple
 fields at once, or used by itself to switch from news or watchlist views to
     chart view.
 ex. {#2ea-fg}$r :6.5h #{/}`
+    const whatQuote = `
+    {bold}{#2ea-fg}help {#cd2-fg}"{/} quote
+Display quote.
+ex. {#2ea-fg}" $de{/}`
     const whatNews = `
     {bold}{#2ea-fg}help {#cd2-fg}!{/} news
 Display news. Can be combined with stock prefixes to update the active symbol
@@ -131,10 +168,16 @@ Display watchlist. Watchlist symbols can be set in config.json`
     const whatProfile = `
     {bold}{#2ea-fg}help {#cd2-fg}&{/} profile
 Display profile`
+    const whatBook = `
+    {bold}{#2ea-fg}help {#cd2-fg}^{/} book
+Display order book data`
+    const whatSectors = `
+    {bold}{#2ea-fg}help {#cd2-fg}sectors{/} sectors
+Display sectors info`
     const whatList = `
     {bold}{#2ea-fg}help {#cd2-fg}*{/} list
 Display lists. You can set which lists you wish to bring back by altering the
-    config.json`
+    config.json. See README`
     const whatSearch = `
     {bold}{#2ea-fg}help {#cd2-fg}?{/} (terms)
 Search for terms by typing a {yello-fg}?{/} and words to look up symbol and
@@ -147,6 +190,17 @@ Target a specfic window by id. Window id is located in the top left corner of
     the component. Can be combined with other commands.
 ex. {#2ea-fg}[2 # $aa :6.5h{/}
 ex. {#2ea-fg}& [1{/}`
+    const whatPoll = `
+    {bold}{#2ea-fg}help {#cd2-fg}poll{/} poll (milliseconds)
+All components can be polled. Components that are polling will refresh
+themselves at the specified interval in milliseconds. {#2ea-fg}poll{/} by
+itself with no ms argument will stop polling in the active window.
+ex. {#2ea-fg}$aa :6.5h poll60000{/}  --> poll once per minute same as 
+ex. {#2ea-fg}poll6e4 $aa :6.5h {/}   --> poll once per minute 
+ex. {#2ea-fg}poll [1{/}              --> stop polling in window 1`
+    const whatAccount = `
+    {bold}{#2ea-fg}help {#cd2-fg}@/account{/} account
+Display information about your iex and alpaca accounts`
     const whatBuy = `
     {bold}{#2ea-fg}help {#cd2-fg}+{/} BUY (quantity) (symbol)
 BUY (quantity) of (symbol)
@@ -162,13 +216,18 @@ Exits iexcli`
     const whats = {
       $: whatSym,
       ':': whatTime,
-      '!': whatNews,
-      '=': whatWatch,
       '#': whatChart,
+      '"': whatQuote,
+      '^': whatBook,
       '&': whatProfile,
       '*': whatList,
-      '?': whatSearch,
+      '!': whatNews,
+      sectors: whatSectors,
+      '=': whatWatch,
       '[': whatWindow,
+      '^': whatPoll,
+      '@': whatAccount,
+      '?': whatSearch,
       '+': whatBuy,
       '-': whatSell,
       exit: whatExit,
